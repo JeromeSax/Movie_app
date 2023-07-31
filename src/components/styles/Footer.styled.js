ffba2e43fcdcc633a125699c3ad995177f901fd3
unknown <jeromesloss@yahoo.com>
2023-07-31 14:22:55 -0400

    footer and styling added

--- src/components/styles/Footer.styled.js
+++ src/components/styles/Footer.styled.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const StyledFooter = styled.footer`
-    background-color: ${({ theme }) => theme.colors.footer};
+    background-color: #0000FF;
     color: #fff;
     padding: 100px 0 60px;
 
